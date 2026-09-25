9c1819126aa6b781653242aab9488379426c4be8
Your Name <sdi2200137@uoa.gr>
2026-09-26 00:43:37 +0300

    separating the projects

---
+++ simple_game_project/programs/game/game.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "raylib.h"
+
+#include "interface.h"
+#include "state.h"
+
+State state;
+
+void update_and_draw() {
+
+    struct key_state keys;
+    keys.down = IsKeyDown(KEY_DOWN) ;
+    keys.enter = IsKeyDown(KEY_ENTER) ;
+    keys.left = IsKeyDown(KEY_LEFT) ;
+    keys.n = IsKeyDown(KEY_N) ;
+    keys.p = IsKeyDown(KEY_P) ;
+    keys.right = IsKeyDown(KEY_RIGHT) ;
+    keys.up = IsKeyDown(KEY_UP) ;
+	state_update(state, &keys);
+	interface_draw_frame(state);
+}
+
+int main() {
+	state = state_create();
+	interface_init();
+
+	// Η κλήση αυτή καλεί συνεχόμενα την update_and_draw μέχρι ο χρήστης να κλείσει το παράθυρο
+	start_main_loop(update_and_draw);
+
+	interface_close();
+
+	return 0;
+}
